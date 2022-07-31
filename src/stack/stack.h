51c5ac97b0498edeed89055bb9f40aa6b5e4faa8
maxim <maxcarlernst2020@gmail.com>
2022-07-31 19:36:25 +0700

    Develop : Added declarations for stack structure functions

--- src/stack/stack.h
+++ src/stack/stack.h
@@ -3,19 +3,20 @@
 
 #include <stdlib.h>
 
-typedef enum {
-    OPERATOR,
-    OPERAND
-} Lexeme_type;
+typedef enum Lexeme_type { OPERATOR, OPERAND } Lexeme_type;
 
-typedef struct {
-    char* lexeme;
-    Lexeme_type type;
+typedef struct Stack_node {
+  char *lexeme;
+  Lexeme_type type;
 } Stack_node;
 
-typedef struct {
-    Stack_node* stack;
-    Stack_node* stack_head;
+typedef struct Stack {
+  Stack_node *stack;
+  Stack_node *stack_head;
 } Stack;
+
+Stack *init_stack(int length);
+Stack_node *pop(Stack *my_stack);
+void push(Stack *my_stack);
 
 #endif // STACK_STACK_H_
