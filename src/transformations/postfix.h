e04fa5bf616e01413e77822c5dfde45a3af317d8
maxim <maxcarlernst2020@gmail.com>
2022-07-31 21:33:53 +0700

    develop: Added declarations for the core functions in transformations

--- src/transformations/postfix.h
+++ src/transformations/postfix.h
@@ -1,5 +1,13 @@
 #ifndef TRANSFORMATIONS_POSTFIX_H_
 #define TRANSFORMATIONS_POSTFIX_H_
 
+#include "../structures/structures.h"
+
+// "+", "-", "*", "/", "^", "sin", "cos", "tan", "ctg", "ln", "(", ")".
+
+int parse(char* expression_string, Expression* infix_expression);
+int transform(Expression* my_expression);
+
+
 
 #endif  // TRANSFORMATIONS_POSTFIX_H_
