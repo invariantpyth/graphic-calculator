3231033ef449ef3ab6364ad60016df0106701d98
Meghan Kinn <meghanki@se-c2.nsk.21-school.ru>
2023-03-08 11:10:21 +0700

    develop: formatted:

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-#include "smartcalc.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    SmartCalc w;
-    w.show();
-    return a.exec();
+#include "smartcalc.h"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+  SmartCalc w;
+  w.show();
+  return a.exec();
 }
